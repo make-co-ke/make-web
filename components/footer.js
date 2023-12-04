cf4c37ec216ed8a3ad50e082744500e3284141d2
Oj-washingtone <jalangowashingtone@gmail.com>
2023-12-04 10:31:47 +0300

    Firstcommit

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer__logo">Logo</div>
+      <div className="footer__menu">Menu items</div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ components/header.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">Logo</div>
+      <div className="menu">Menu items</div>
+    </div>
+  );
+}
+
+export default Header;
