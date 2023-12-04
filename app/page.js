7607ffea36a9534dc4157cc0cc1085dc6551682d
Oj-washingtone <jalangowashingtone@gmail.com>
2023-12-04 14:32:53 +0300

    what we do section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import "@styles/globals.css";
 import Image from "next/image";
 import Link from "next/link";
+import boxicons from "boxicons/css/boxicons.min.css";
 
 function Home() {
   return (
     <div class="container mx-auto px-4">
       <div class="columns-2 __barner">
-        <div className="w-full aspect-square __intro">
+        <div className="w-full  __intro">
           <p className="intro__text">
             Transforming vision
             <br />
@@ -20,12 +21,43 @@ function Home() {
             Get started
           </Link>
         </div>
-        <div className="w-full aspect-square __intro __intro__image__wrapper ">
+        <div className="w-full __intro __intro__image__wrapper ">
           <Image src="/assets/images/3d__1.png" width={391} height={407} />
         </div>
       </div>
+      <div class="__title_section">
+        <h1 className="__heading__2">What we do.</h1>
+        <p className="__title__description">
+          We are transforming vision into a digital reality
+        </p>
+        <div className="columns-3 __services">
+          <div className="__service__card">
+            <div className="icon"></div>
+            <p className="__service__title">Innovative Product Development</p>
+            <p className="__text__natural">
+              Crafting cutting-edge solutions through our own product
+              innovations, staying at the forefront of technology trends.
+            </p>
+          </div>
+          <div className="__service__card">
+            <p className="__service__title">Software Development</p>
+            <p className="__text__natural">
+              We partner with businesses to transform their ideas into a digital
+              reality. We build the technology needed for sustained growth and
+              success.
+            </p>
+          </div>
+          <div className="__service__card">
+            <p className="__service__title">Search Engine Optimization</p>
+            <p className="__text__natural">
+              Collaborating with businesses ensuring seamless execution of their
+              digital strategies while optimizing their online presence with
+              strategic SEO from the ground u
+            </p>
+          </div>
+        </div>
+      </div>
 
-      <div class="row">What we do</div>
       <div class="row">Unseen potential </div>
       <div class="row">Statistics</div>
       <div class="row">Tech stacks</div>
